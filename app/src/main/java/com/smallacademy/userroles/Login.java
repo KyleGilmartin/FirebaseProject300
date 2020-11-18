8198c0b2ab37c2e69c6c775e9461a8ec3919ef3c
kyle gilmartin <46896160+kyle-gilmartin@users.noreply.github.com>
2020-11-18 12:18:35 +0000

    Fire commit

--- app/src/main/java/com/smallacademy/userroles/Login.java
+++ app/src/main/java/com/smallacademy/userroles/Login.java
@@ -1,29 +1,68 @@
 package com.smallacademy.userroles;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class Login extends AppCompatActivity {
     EditText email,password;
     Button loginBtn,gotoRegister;
     boolean valid = true;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
         email = findViewById(R.id.loginPassword);
         password = findViewById(R.id.loginPassword);
         loginBtn = findViewById(R.id.loginBtn);
         gotoRegister = findViewById(R.id.gotoRegister);
 
-        checkField(email);
-        checkField(password);
+       loginBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               checkField(email);
+               checkField(password);
+
+               if(valid){
+                    fAuth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+               }
+           }
+       });
+
+        gotoRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+            }
+        });
 
 
 
@@ -40,5 +79,16 @@ public class Login extends AppCompatActivity {
         }
 
         return valid;
+    }
+
+
+    // to check to see if an user is login in or not
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser() !=null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
     }
 }
--- app/src/main/java/com/smallacademy/userroles/Register.java
+++ app/src/main/java/com/smallacademy/userroles/Register.java
@@ -1,20 +1,40 @@
 package com.smallacademy.userroles;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
     EditText fullName,email,password,phone;
     Button registerBtn,goToLogin;
     boolean valid = true;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        fAuth  = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         fullName = findViewById(R.id.registerName);
         email = findViewById(R.id.registerEmail);
@@ -23,12 +43,53 @@ public class Register extends AppCompatActivity {
         registerBtn = findViewById(R.id.registerBtn);
         goToLogin = findViewById(R.id.gotoLogin);
 
-        checkField(fullName);
-        checkField(email);
-        checkField(password);
-        checkField(phone);
+       registerBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               checkField(fullName);
+               checkField(email);
+               checkField(password);
+               checkField(phone);
+
+               if(valid){
+                   fAuth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                       @Override
+                       public void onSuccess(AuthResult authResult) {
+                           FirebaseUser user = fAuth.getCurrentUser();
+                           Toast.makeText(Register.this,"Account Created",Toast.LENGTH_SHORT).show();
+                           DocumentReference df = fStore.collection("Users").document(user.getUid());
+                           Map<String,Object> userinfo = new HashMap<>();
+                           userinfo.put("FullName",fullName.getText().toString());
+                           userinfo.put("UserEmail",email.getText().toString());
+                           userinfo.put("PhoneNumber",phone.getText().toString());
+
+                           // specify if user is admin
+                           userinfo.put("isUser","1");
+                           df.set(userinfo);
+
+                           startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                           finish();
+                       }
+                   }).addOnFailureListener(new OnFailureListener() {
+                       @Override
+                       public void onFailure(@NonNull Exception e) {
+                           Toast.makeText(Register.this,"Failed to created account",Toast.LENGTH_SHORT).show();
+                       }
+                   });
+               }
+           }
+       });
+
+       goToLogin.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               startActivity(new Intent(getApplicationContext(),Login.class));
+           }
+       });
 
     }
+
+
 
     public boolean checkField(EditText textField){
         if(textField.getText().toString().isEmpty()){
